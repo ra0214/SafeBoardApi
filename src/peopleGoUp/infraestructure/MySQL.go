eeaca87843481d155ee130d57805de4a9329f4fa
christianramirez <cm2849436@gmail.com>
2025-04-04 11:40:53 -0600

    no

--- src/peopleGoUp/infraestructure/MySQL.go
+++ src/peopleGoUp/infraestructure/MySQL.go
@@ -43,27 +43,26 @@ func (mysql *MySQL) SavePeopleGoUp(esp32ID string, cantidad int32) error {
 	return nil
 }
 
-// Obtener todos los registros de conteo de personas
 func (mysql *MySQL) GetAll() ([]domain.PeopleGoUp, error) {
 	query := "SELECT id, esp32_id, conteo FROM goup"
 	rows, err := mysql.conn.FetchRows(query)
 	if err != nil {
-		return nil, fmt.Errorf("error al ejecutar la consulta SELECT: %v", err)
+		return nil, fmt.Errorf("Error al ejecutar la consulta SELECT: %v", err)
 	}
 	defer rows.Close()
 
-	var peopleGoUpList []domain.PeopleGoUp
+	var peopleGoUpp []domain.PeopleGoUp
 
 	for rows.Next() {
 		var peopleGoUp domain.PeopleGoUp
 		if err := rows.Scan(&peopleGoUp.ID, &peopleGoUp.Esp32ID, &peopleGoUp.Conteo); err != nil {
-			return nil, fmt.Errorf("error al escanear la fila: %v", err)
+			return nil, fmt.Errorf("Error al escanear la fila: %v", err)
 		}
-		peopleGoUpList = append(peopleGoUpList, peopleGoUp)
+		peopleGoUpp = append(peopleGoUpp, peopleGoUp)
 	}
 
 	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("error iterando sobre las filas: %v", err)
+		return nil, fmt.Errorf("Error iterando sobre las filas: %v", err)
 	}
-	return peopleGoUpList, nil
+	return peopleGoUpp, nil
 }
